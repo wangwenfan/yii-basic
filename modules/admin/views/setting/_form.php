2719ce00e8378aebc26dbea9ad1015337e306476
wangwf <787802868@qq.com>
2017-05-23 21:29:50 +0800

    xiugai form

--- modules/admin/views/setting/_form.php
+++ modules/admin/views/setting/_form.php
@@ -8,15 +8,15 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="setting-form">
+<div class="setting-form col-lg-8">
 
     <?php $form = ActiveForm::begin(); ?>
 
     <?= $form->field($model, 'sitename')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'siteremark')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'status')->textInput() ?>
+    <? if($model->status == '') $model->status = 1; ?>
+    <?= $form->field($model, 'status')->radioList(['1'=>'启用','0'=>'禁用']) ?>
 
     <?= $form->field($model, 'sitelink')->textInput(['maxlength' => true]) ?>
 
